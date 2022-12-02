046fc1d0cc681ca8bc2a6e7a54c538f5e698cee4
iamogbz <iamogbz+github@gmail.com>
2022-12-02 06:37:39 -0500

    chore: wip

--- script.js
+++ script.js
@@ -27,28 +27,60 @@ function createGrid() {
         (Math.abs(GRID_COL_COUNT / 2 - c) / GRID_COL_COUNT +
           Math.abs(GRID_ROW_COUNT / 2 - r) / GRID_ROW_COUNT);
       return {
-        magnitude: scale * MAX_MAGNITUDE,
-        angle: scale * Math.PI * 2,
-        temp: scale * MAX_TEMPERATURE,
+        mag: scale * MAX_mag,
+        ang: scale * Math.PI * 2,
+        tmp: scale * MAX_tmpERATURE,
       };
     })
   );
 }
 
 function updateGrid(gridValues) {
+  const UPDATE_SPEED = 10;
+  const previousGridValues = deepClone(gridValues);
+
   gridValues.forEach((row, rowIdx) => {
     row.forEach((cell, colIdx) => {
       // get the average values from surrounding cells
       const neighbourCells = getAverageNeighbourCell(
-        gridValues,
+        previousGridValues,
         rowIdx,
         colIdx
       );
+      cell.mag += (neighbourCells.mag - cell.mag) / UPDATE_SPEED;
+      cell.ang += (neighbourCells.ang - cell.ang) / UPDATE_SPEED;
+      cell.tmp += (neighbourCells.tmp - cell.tmp) / UPDATE_SPEED;
     });
   });
 }
 
-function getAverageNeighbourCell(gridValues, rowIdx, colIdx) {}
+function deepClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
+
+function getAverageNeighbourCell(gridValues, rowIdx, colIdx) {
+  const avgNCell = [
+    [rowIdx - 1, colIdx - 1],
+    [rowIdx, colIdx - 1],
+    [rowIdx + 1, colIdx - 1],
+    [rowIdx - 1, colIdx],
+    [rowIdx + 1, colIdx],
+    [rowIdx - 1, colIdx + 1],
+    [rowIdx, colIdx + 1],
+    [rowIdx + 1, colIdx + 1],
+  ].reduce(
+    (acc, [r, c]) => ({
+      mag: acc.mag + gridValues[r]?.[c]?.mag || 1,
+      ang: acc.ang + gridValues[r]?.[c]?.ang || Math.PI * 2,
+      tmp: acc.tmp + gridValues[r]?.[c]?.tmp || 1,
+    }),
+    { mag: 0, ang: 0, tmp: 0 }
+  );
+  avgNCell.mag /= 8;
+  avgNCell.ang /= 8;
+  avgNCell.tmp /= 8;
+  return avgNCell;
+}
 
 function renderGrid(gridValues, canvasCtx) {
   gridValues.forEach((row, rowIdx) => {
@@ -71,18 +103,18 @@ function renderGrid(gridValues, canvasCtx) {
       const centerX = startX + cellBoundSize / 2;
       const centerY = startY + cellBoundSize / 2;
       canvasCtx.translate(centerX, centerY);
-      canvasCtx.rotate(cell.angle);
+      canvasCtx.rotate(cell.ang);
       // canvasCtx.fillStyle = '#000';
       // canvasCtx.fillRect(0, 0, 2, 2);
-      canvasCtx.fillStyle = `#${getTempColor(cell.temp)}`;
-      const cellSize = (cell.magnitude / MAX_MAGNITUDE) * CELL_WIDTH_PX;
+      canvasCtx.fillStyle = `#${gettmpColor(cell.tmp)}`;
+      const cellSize = (cell.mag / MAX_mag) * CELL_WIDTH_PX;
       canvasCtx.fillRect(
         -cellSize / 2,
         -CELL_HEIGHT_PX / 2,
         cellSize,
         CELL_HEIGHT_PX
       );
-      canvasCtx.rotate(-cell.angle);
+      canvasCtx.rotate(-cell.ang);
       canvasCtx.translate(-centerX, -centerY);
     });
   });
@@ -91,8 +123,8 @@ function renderGrid(gridValues, canvasCtx) {
 /**
  * Convert value between 0..1 to 3 char hexadecimal e.g. 1 => FFF
  */
-function getTempColor(temp) {
-  return Math.round((temp / MAX_TEMPERATURE) * 4095).toString(16);
+function gettmpColor(tmp) {
+  return Math.round((tmp / MAX_tmpERATURE) * 4095).toString(16);
 }
 
 const MAIN_CANVAS_ID = 'main-screen';
@@ -101,7 +133,7 @@ const GRID_ROW_COUNT = 20;
 const CELL_MARGIN_PX = 1;
 const CELL_HEIGHT_PX = 2;
 const CELL_WIDTH_PX = 20;
-const MAX_MAGNITUDE = 2;
-const MAX_TEMPERATURE = 1;
+const MAX_mag = 2;
+const MAX_tmpERATURE = 1;
 
 main();
